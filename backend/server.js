8e81e947c73d9e763807d7bbb068417d8820bab8
JannatulHappy <jannatulferdoushappy21@gmail.com>
2022-05-13 15:12:08 +0600

    mongooseConnected

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
-
-
+const connectDB = require("./config/db");
 dotenv.config();
+connectDB();
+
+const app = express();
 const PORT = process.env.PORT || 5000;
-
-const app = express()
-app.get("/",(req,res)=>{
-    res.send("server created successfully")
-})
-app.get("/api/chat",(req,res)=>{
-    res.send(chats)
-
-})
-app.get("/api/chat/:id",(req,res)=>{
-    // console.log(req.params.id)
-    const singleChat = chats.find((chat)=>chat._id===req.params.id)
-    res.send(singleChat)
-})
+app.get("/", (req, res) => {
+  res.send("server created successfully");
+});
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+app.get("/api/chat/:id", (req, res) => {
+  // console.log(req.params.id)
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
+});
 app.listen(PORT, () => {
-    console.log(`Listening to port : ${PORT}`);
-  });
+  console.log(`Listening to port : ${PORT}`);
+});
