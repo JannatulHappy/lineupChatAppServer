48829092923d4c911039f784ad48bdf30885b98d
JannatulHappy <jannatulferdoushappy21@gmail.com>
2022-05-13 11:40:57 +0600

    express-server-created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
 
-const port = process.env.PORT || 5000;
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express()
 app.get("/",(req,res)=>{
-    res.send("server created")
+    res.send("server created successfully")
 })
-app.listen(port, () => {
-    console.log(`Listening to port : ${port}`);
+app.get("/api/chat",(req,res)=>{
+    res.send(chats)
+
+})
+app.get("/api/chat/:id",(req,res)=>{
+    // console.log(req.params.id)
+    const singleChat = chats.find((chat)=>chat._id===req.params.id)
+    res.send(singleChat)
+})
+app.listen(PORT, () => {
+    console.log(`Listening to port : ${PORT}`);
   });
